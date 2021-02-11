2ab537b8bfb54a39ae98b0c4c1d4b46c061f5347
InFinity_54 <corentinkreicher@gmail.com>
2021-02-11 11:51:24 +0100

    Création du projet

---
+++ CESI_CSharp_TPChocolatine/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CESI_CSharp_TPChocolatine.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
